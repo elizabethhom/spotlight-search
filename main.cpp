5fb0ffa7175a27109bf1b4d270bf3e4808596c27
elizabethhom <59620870+elizabethhom@users.noreply.github.com>
2020-08-10 00:08:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-/********************************************
-* COMP15 - FA2019
-* PROJECT 2
-* ELIZABETH HOM
-* 6 DEC 2019
-* 
-* main.cpp
-*
-* interactive main for gerp
-*********************************************/
+/*
+ * main.cpp
+ * 
+ * COMP15 - FALL2019
+ * Elizabeth Hom (ehom01)
+ * Last Modified: December 6, 2019
+ * 
+ * Purpose: Main driver for spotlight-search. Drives the traversal of
+ *          file tree and population of hash table structure, and runs
+ *          continuous user-driven query.
+ */
 
 #include <iostream>
 #include <vector>
@@ -30,8 +31,10 @@ int main(int argc, char *argv[])
 {
     ifstream input;
     ofstream output;
-    vector<string> paths;   // vector containing paths to all files from given
-    int hashNumElem = 0;    // start directory
+    
+    // Vector of paths to all files within file tree
+    vector<string> paths;
+    int hashNumElem = 0;
 
     if (argc == 3) {
         string dirName = argv[1];
@@ -41,31 +44,36 @@ int main(int argc, char *argv[])
         hashTable *table = new hashTable();
         savePaths(tree->getRoot(), paths, dirName);
 
-        // gets all words from every file, hashes each one into hash table
+        // Obtains all words from each file, hashes into hash table
         for(unsigned long i = 0; i < paths.size(); i++) {
             input.open(paths[i]);
             hashNumElem = getWords(input, table, paths[i], hashNumElem);
             input.close();
         }
-        getQueries(table, output);  // continuously gets user queries and
-        output.close();             // acts accordingly
+        
+        // Continuously obtains user queries and acts accordingly
+        getQueries(table, output);
+        output.close();
         delete tree;
         delete table;
         paths.clear();
     } else {
-        cout << "Usage:  gerp directory output_file" << endl;
+        cout << "Usage: ./gerp <directory> <output_file>" << endl;
         return EXIT_FAILURE;
     }
 
     return 0;
 }
 
-// function    savePaths
-// parameters: pointer to a node (root), vector containing paths to all files
-//             from the starting node, name of starting directory
-// returns:    none
-// does:       saves full path name of all files reachable from the starting
-//             directory into paths vector
+/*
+ * savePaths
+ *
+ * Parameters: Pointer to node (root), vector containing file tree paths, name
+ *             of start directory (string)
+ * Returns:    NA
+ * Does:       Saves full path name of all files within file tree into paths
+ *             vector
+ */
 void savePaths(DirNode *node, vector<string> &paths, string dirName)
 {
     if (node->hasFiles()) {
@@ -86,42 +94,50 @@ void savePaths(DirNode *node, vector<string> &paths, string dirName)
     }
 }
 
-// function    getWords
-// parameters: ifstream, hashTable, file path, number of elements hashed thus
-//             far
-// returns:    number of elements in the file passed in
-// does:       for a given file, reads in individual words in file until reach
-//             end of file. for each word, its lowercase version is hashed into
-//             the table
+/*
+ * getWords
+ *
+ * Parameters: input stream (ifstream), hashTable, file path (string), current
+ *             number of hashed elements (int)
+ * Returns:    Number of elements in file
+ * Does:       For a given file, reads word by word. For each word, lowercase
+ *             version is hashed into table.
+ */
 int getWords(istream &input, hashTable *table, string path, int numElem)
 {
     string currLine, lower, orig;
     int fileLine = 1;
-    vector<string> temp;    // vector that holds all words on a line
+    // Temp vector that holds all words on current line
+    vector<string> temp;
 
     while(not input.eof()) {
         getline(input, currLine);
         temp.clear();
         istringstream ss(currLine);
-        while(ss >> orig) {         // parses individual words on line
-            orig = stripNonAlphaNum(orig);  // strip nonAlphaNum chars
+        // Parses individual words on current line
+        while(ss >> orig) {
+            orig = stripNonAlphaNum(orig);  // Strips nonAlphaNum chars
             bool found = false;
 
-            if (temp.size() == 0) {     // always push first word in line
-               temp.push_back(orig);    // into temp vector, hash into
-               lower = table->lowercase(orig);  // table
+            // Always push first word in line into temp vector, hash into table
+            if (temp.size() == 0) {
+               temp.push_back(orig);
+               lower = table->lowercase(orig);
                numElem++;
                table->hashWord(lower, orig, fileLine, path, numElem);
             } else {
                 for(unsigned long i = 0; i < temp.size(); i++) {
-                    if (temp[i] == orig) {   // if line has the same word
-                        found = true;        // multiple times, do not add into
-                        break;               // temp vector
+                    /* If line has same word multiple times, do not add into
+                       temp vector */
+                    if (temp[i] == orig) {
+                        found = true;
+                        break;
                     }
                 }
 
-                if (not found) {            // otherwise, add to temp vector
-                    temp.push_back(orig);   // and hash into table
+                // Otherwise, add to temp vector and hash into table
+                if (not found) {
+                    temp.push_back(orig);
                     lower = table->lowercase(orig);
                     numElem++;
                     table->hashWord(lower, orig, fileLine, path, numElem);
@@ -134,12 +150,13 @@ int getWords(istream &input, hashTable *table, string path, int numElem)
     return numElem;
 }
 
-// function    getQueries
-// parameters: hashTable ofstream
-// returns:    none
-// does:       continuously prompts for user input and program acts
-//             accordingly (@q = quit program, @f = change file, @i = case
-//             insensitive search, otherwise performs case sensitive search)
+/*
+ * getQueries
+ *
+ * Parameters: hash table, output stream (ofstream)
+ * Returns:    NA
+ * Does:       Continuously prompts user input and reacts accordingly
+ */
 void getQueries(hashTable *table, ofstream &output)
 {
     string command, queryWord;
@@ -151,27 +168,31 @@ void getQueries(hashTable *table, ofstream &output)
         if (command == "@q" or command == "@quit") {
             cout << "Goodbye! Thank you have a nice day." << endl;
             break;
-        } else if (command == "@f") {   // change outputFile
+        } else if (command == "@f") {
+            // Change output file
             string newOutputFile;
             cin >> newOutputFile;
             output.close();
             output.open(newOutputFile);
-        // case insensitive search
         } else if (command == "@i" or command == "@insensitive") {
+            // Case insensitive search
             cin >> queryWord;
             table->findWordInsensitive(stripNonAlphaNum(queryWord), output);
-        // case sensitive search
         } else {
+            // Case sensitive search
             table->findWord(stripNonAlphaNum(command), output);
         }
     } while (command != "@q" or command != "@quit");
 }
 
-// function    isAlphaNum
-// parameters: a character, passed in from stripNonAlphaNum()
-// returns:    true if the character is alpha-numeric, false if not
-// does:       a helper function that assesses whether the passed in character
-//             is alpha-numeric using ASCII values
+/*
+ * isAlphaNum
+ *
+ * Parameters: A character, passed in from stripNonAlphaNum()
+ * Returns:    Boolean, true if character is alpha-numeric, otherwise false
+ * Does:       Assesses whether passed in character is alpha-numeric using
+ *             ASCII values.
+ */
 bool isAlphaNum(char temp)
 {
     if (temp >= 48 and temp <= 57)
@@ -184,11 +205,14 @@ bool isAlphaNum(char temp)
         return false;
 }
 
-// function    stripNonAlphaNum
-// parameters: a string intended to be processed
-// returns:    the processed string, outputString
-// does:       removes all leading and trailing non-alphanumeric characters
-//             from a passed in inputString
+/*
+ * stripNonAlphaNum
+ *
+ * Parameters: word to be processed (string)
+ * Returns:    Processed string
+ * Does:       Removes all leading and trailing non-alphanumeric characters
+ *             from a passed in input string
+ */
 string stripNonAlphaNum(string inputString)
 {
     string outputString = "";
@@ -196,21 +220,23 @@ string stripNonAlphaNum(string inputString)
     int firstIndex = 0;
     int lastIndex = inputString.size() - 1;
 
+    // Iterates through every character in string
     for (unsigned long i = 0; i < inputString.length(); i++) {
-        char temp = inputString[i];     // iterates thru every char in string
+        char temp = inputString[i];
 
-        if (isAlphaNum(temp)) {         // if char is alphanumeric
+        if (isAlphaNum(temp)) {
             if (not foundFirst) {
                 foundFirst = true;
-                firstIndex = i;         // saves index of 1st alphanumeric char
+                firstIndex = i;         // Saves index of 1st alphanumeric char
             }
-            lastIndex = i;              // index of last alphanumeric char
+            lastIndex = i;              // Saves index of last alphanumeric char
         }
     }
 
-    for (int i = firstIndex; i < lastIndex + 1; i++) {  // parses out leading &
-        char temp = inputString[i];                     // trailing nonalphanum
-        outputString = outputString + temp;             // characters from str
+    // Parses out leading and trialing non-alphanumeric chars from string
+    for (int i = firstIndex; i < lastIndex + 1; i++) {
+        char temp = inputString[i];
+        outputString = outputString + temp;
     }
 
     return outputString;
